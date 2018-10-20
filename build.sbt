77612f18a5939cc69a7679ad87e595d77158d846
Krzysztof Langner <klangner@gmail.com>
2018-10-20 23:12:35 +0200

    init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "figaro-sandbox"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
---
+++ src/main/scala/sandbox/ABTestApp.scala
@@ -0,0 +1,8 @@
+package sandbox
+
+object ABTestApp {
+
+  def main(args: Array[String]): Unit = {
+    println("A/B hypothesis Testing")
+  }
+}
